de2b0a02650ca6f840e14b0b1b1cc126cbb7e772
!t  ¡Sn't <karthikhr1623@gmail.com>
2025-05-30 09:58:21 -0400

    Add files via upload

--- banner/banner.go
+++ banner/banner.go
@@ -1,1 +1,22 @@
-banner/banner.go
+package banner
+
+import "fmt"
+
+func PrintBanner() {
+	// ANSI escape hyperlink format: \x1b]8;;URL\x07TEXT\x1b]8;;\x07
+	link := "\x1b]8;;https://github.com/Karthik-HR0/onlyjs\x07Karthik-HR0\x1b]8;;\x07"
+	fmt.Println(`
+  ________     __        
+ /  _____/    |__| ______
+/   \  ___    |  |/  ___/
+\    \_\  \   |  |\___ \ 
+ \______  /\__|  /____  >
+        \/ \______|    \/        v0.1-alpha
+        
+                             ` + link + `
+	`)
+}
+
+func main() {
+	PrintBanner()
+}
